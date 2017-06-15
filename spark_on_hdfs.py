b145bf611c2eb31bef0ae3884f5d96b34c16b7fb
谢恩泽 <Johnny_ez@163.com>
2017-06-15 20:00:35 +0800

    spark on hdfs
    spark  从  hdfs中读数据

---
+++ spark_on_hdfs.py
@@ -0,0 +1,31 @@
+'''
+启动hdfs后执行 bin/spark-submit /Users/xieenze/Desktop/spark/wc.py 'hdfs://localhost:9000/test.txt'
+'''
+
+from __future__ import print_function
+
+import sys
+from operator import add
+
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: wordcount <file>", file=sys.stderr)
+        exit(-1)
+
+    spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+
+    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    counts = lines.flatMap(lambda x: x.split(' ')) \
+                  .map(lambda x: (x, 1)) \
+                  .reduceByKey(add)
+    output = counts.collect()
+    for (word, count) in output:
+        print("%s: %i" % (word, count))
+
+    spark.stop()
